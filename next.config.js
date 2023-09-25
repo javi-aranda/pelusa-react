8907275a57dbd9ced6d7ce63d4b00c1f5bf6facb
Javier Aranda Santos <javier.aranda.designs@gmail.com>
2023-09-25 19:31:07 +0200

    Request to backend

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env: {
+        API_ENDPOINT: process.env.API_ENDPOINT,
+    }
+}
 
 module.exports = nextConfig
