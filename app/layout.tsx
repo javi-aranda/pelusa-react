06d167f5671b222d7b81d0dc854fe61b20c55db5
Javier Aranda Santos <javier.aranda.designs@gmail.com>
2023-09-27 20:52:49 +0200

    Layout more responsive

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <section className="w-full h-screen py-12 md:py-24 lg:py-32 xl:py-48 bg-gray-800">
-          <div className="container px-4 md:px-6">
+          <div className="container mx-auto px-4 md:px-6">
             <div className="grid gap-6 items-center">
               <div className="flex flex-col justify-center space-y-4 text-center">
                 {children}
